5aacc25ff847276da3a98159a94422dd872f18a2
Ivan Stepaniuk <ivan@thecloakroom.nl>
2015-05-06 17:05:05 +0200

    better version of split

--- split/split.sh
+++ split/split.sh
@@ -1,10 +1,39 @@
-PARTITIONS=$1
+#!/bin/bash
+
+PARTS=$1
 IMAGE=$2
 
-cat $PARTITIONS | while read line; do
-    start=$((`echo $line|cut -d' ' -f1`))
-    end=$((`echo $line|cut -d' ' -f2`))
-    name=$(basename $IMAGE)-$(echo $line|cut -d ' ' -f3)
-    count=$(($end - $start))
-    echo dd if=$IMAGE of=$name skip=$start count=$count bs=1024 iflag=skip_bytes,count_bytes
-done
+if [ ! -f "$IMAGE" ] || [ ! -f $PARTS ] ; then
+
+    cat << EOF
+Usage: $0 PARTITION_LIST_FILE IMAGE_FILE
+
+The PARTITION_LIST_FILE must contain, one partition per line,
+offsets must be provided in hexadecimal:
+START_OFFSET END_OFFSET PARTITION_NAME
+
+ie:
+0x00000000 0x00020000 bootloader
+0x00030000 0x000f0000 kernel
+
+WARNING: output files will be named IMAGE_FILE-PARTITION_NAME
+and will be OVERWRITTEN.
+EOF
+    exit
+fi
+
+IMAGE_BASENAME=$(basename $IMAGE)
+
+while read line; do
+    offset=$(echo $line|cut -d' ' -f1)
+    end=$(echo $line|cut -d' ' -f2)
+    name=$(echo $line|cut -d' ' -f3)
+    size=$(($end - $offset))
+    if [[ size -le 0 ]]; then
+        echo "ERROR: Negative or null size for partition: $line"
+        exit
+    fi
+    echo "Extracting partition '$name' [$offset:$end] ($size bytes)"
+    dd iflag=skip_bytes,count_bytes if="$IMAGE" bs=1M \
+        of="$IMAGE_BASENAME-$name" skip=$(($offset)) count=$size
+done < $PARTS
